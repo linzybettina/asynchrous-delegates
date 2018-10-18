c9dcc823e0bfa33682c7f572494eb9096ccd31c2
linzybettina <43557782+linzybettina@users.noreply.github.com>
2018-10-18 22:35:25 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+namespace AsyncCallbackDelegate
+
+{
+
+    public delegate int MyDelegate(int x);
+
+    public class MyClass
+
+    {
+
+        //A method to be invoke by the delegate
+
+        public int MyMethod(int x)
+
+        {
+
+            //simulate a long running proccess
+
+            Thread.Sleep(10000);
+
+            return x * x;
+
+        }
+
+    }
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            MyClass myClass1 = new MyClass();
+
+            MyDelegate del = new MyDelegate(myClass1.MyMethod);
+
+            //Invoke our methe in another thread
+
+            IAsyncResult async = del.BeginInvoke(5, new AsyncCallback(MyCallBack), null);
+
+            Console.WriteLine("Proccessing the Operation....");
+
+            Console.ReadLine();
+
+        }
+
+        static void MyCallBack(IAsyncResult async)
+
+        {
+
+            Console.WriteLine("Operation Complete");
+
+        }
+
+    }
+
+}
